5799c8d1a2e0c173a6a8b8aaf94214e9571a578f
Ahmed Hajeyah <hajeyah@gmail.com>
2022-06-11 16:06:54 +0300

    Finishing the Task

--- challenge1.dart
+++ challenge1.dart
@@ -4,6 +4,14 @@
  * - that just prints your name on the screen
  */
 
+// void main() {
+//   printName();
+// }
+
+// void printName() {
+//   print("My Name is Ahmad Hajeyah!");
+// }
+
 /**
  * Task 2:
  * Create a function named `printAge`
@@ -11,6 +19,14 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+
+// void main() {
+//   printAge(1982);
+// }
+
+// void printAge(int birth) {
+//   print(2022 - birth);
+// }
 
 /**
  * Task 3:
@@ -23,9 +39,38 @@
  * -- tr: it should print `Merhaba NAME`
  */
 
+// void main(){
+
+// }
+
+// void printHello(String name,String language) {
+//   if (language == "en") {
+//     print("Hello $name")
+//   }else  if (language == "es") {
+//     print("Hola $name");
+//   }else  if (language == "fr") {
+//     print("Bonjour $name");
+//   }else  if (language == "tr") {
+//     print("Merhaba $name");
+//   }
+// }
+
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+void main() {
+  printMax(10, -1);
+}
+
+void printMax(int x, int y) {
+  if (x > y) {
+    print(x);
+  } else if (y > x) {
+    print(y);
+  } else {
+    print(x);
+  }
+}
